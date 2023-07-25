7669b51f675623973e9da5aeb19c60591532a42e
Jack Myers <jmyers@jackmyers.one>
2023-07-26 04:25:00 +0800

    Add a simple "tap-to-place-prefab" script

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+public class TapToPlace : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabToSpawn;
+
+    [SerializeField]
+    private ARRaycastManager raycastManager;
+
+    [SerializeField]
+    private ARPlaneManager planeManager;
+
+    private List<ARRaycastHit> hits = new();
+
+    private void OnEnable()
+    {
+        EnhancedTouch.TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.TouchSimulation.Disable();
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        if (finger.index != 0) return;
+
+        if (raycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var pose = hits[0].pose;
+            var _ = Instantiate(prefabToSpawn, pose.position, pose.rotation);
+        }
+    }
+}
